d98e4aa62d332fc7c62197d5e0a36cc099f9bf11
Sargam <59954051+sargamVyas@users.noreply.github.com>
2020-09-22 23:44:02 +0200

    Update ChicagoStyleVeggiePizza.java

--- Factory-Pattern/src/com/producer/types/ChicagoStyleVeggiePizza.java
+++ Factory-Pattern/src/com/producer/types/ChicagoStyleVeggiePizza.java
@@ -4,13 +4,13 @@ public class ChicagoStyleVeggiePizza extends Pizza {
 	
 	public ChicagoStyleVeggiePizza()
 	{
-		name="Chicago Style Veggie Pizza";
-		dough="Thick crust pizza";
-		sauce="Thick layer of tomato sauce";
+		name = "Chicago Style Veggie Pizza";
+		dough = "Thick crust pizza";
+		sauce = "Thick layer of tomato sauce";
 		
-		topping .add("Aubergine");
-		topping .add("Corn");
-		topping .add("Carrot");
+		topping.add("Aubergine");
+		topping.add("Corn");
+		topping.add("Carrot");
 	}
 
 }
