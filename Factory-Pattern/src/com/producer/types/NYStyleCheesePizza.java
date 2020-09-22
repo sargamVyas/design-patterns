e36d53ba8dd24e438da2a171365f7db5059682ec
Sargam <59954051+sargamVyas@users.noreply.github.com>
2020-09-22 23:44:30 +0200

    Update NYStyleCheesePizza.java

--- Factory-Pattern/src/com/producer/types/NYStyleCheesePizza.java
+++ Factory-Pattern/src/com/producer/types/NYStyleCheesePizza.java
@@ -4,9 +4,9 @@ public class NYStyleCheesePizza extends Pizza {
 	
 	public NYStyleCheesePizza()
 	{
-		name="New York Style Cheese Pizza";
-		dough="Thin Crust dough";
-		sauce="Thin layer of tomato sauce";
+		name = "New York Style Cheese Pizza";
+		dough = "Thin Crust dough";
+		sauce = "Thin layer of tomato sauce";
 		
 		topping.add("Grated Reggiano Cheese");
 	}
