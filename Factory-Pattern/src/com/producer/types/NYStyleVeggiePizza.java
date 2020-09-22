2d16b81bb72f42e0702b1920e4525f2b88718a37
Sargam <59954051+sargamVyas@users.noreply.github.com>
2020-09-22 23:42:38 +0200

    Update NYStyleVeggiePizza.java

--- Factory-Pattern/src/com/producer/types/NYStyleVeggiePizza.java
+++ Factory-Pattern/src/com/producer/types/NYStyleVeggiePizza.java
@@ -4,9 +4,9 @@ public class NYStyleVeggiePizza extends Pizza {
 	
 	public NYStyleVeggiePizza()
 	{
-		name="New York Style Veg Pizza";
-		dough="Thin Crust dough";
-		sauce="Thin layer of tomato sauce";
+		name = "New York Style Veg Pizza";
+		dough = "Thin Crust dough";
+		sauce = "Thin layer of tomato sauce";
 		
 		topping.add("Tomato");
 		topping.add("Mushroom");
